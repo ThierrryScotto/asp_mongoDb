b00c7a94c2e13f55046d105df1b063eb0d8335fd
Thierry <37625040+ThierrryScotto@users.noreply.github.com>
2020-03-12 22:40:04 -0300

    Add files via upload

---
+++ BooksApi/BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BooksApi.Services;
+using BooksApi.Models;
+using Microsoft.AspNetCore.Routing;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+        public BooksController (BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return _bookService.Get();
+        }
+
+        [HttpGet("{id}", Name = "GetBook")]
+        public ActionResult<Book> Get(String id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<List<Book>> Create(Book book)
+        {
+            _bookService.Create(book);
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Update(id, bookIn);
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Remove(book.Id);
+            return NoContent();
+        }
+    }
+}
---
+++ BooksApi/BooksApi/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] 
+        public String Id { get; set; }
+
+        [BsonElement("Name")]
+        public String Name { get; set; }
+
+        [BsonElement("Price")]
+        public Decimal Price { get; set; }
+
+        [BsonElement("Category")]
+        public String Category { get; set; }
+
+        [BsonElement("Author")]
+        public String Author { get; set; }
+    }
+}
